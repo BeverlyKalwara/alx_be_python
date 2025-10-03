60a277816eb660d4bbb79b6a8ccb31136d19e319
BeverlyKalwara <beverlykluvaga@gmail.com>
2025-10-03 22:57:09 +0300

    Creating Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -21,7 +21,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 != 0:
                 return num1 / num2
-            if num2 == 0:
+            else:
                 return "Cannot divide by zero."
         case _:
             return "Invalid operation."
