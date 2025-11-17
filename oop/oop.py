8c1a83d461e4a470bec59c33e359e39332e516e5
BeverlyKalwara <beverlykluvaga@gmail.com>
2025-11-17 12:35:44 +0300

    Distinguishing Between Class Methods and Static Methods

--- oop/oop.py
+++ oop/oop.py
@@ -10,10 +10,16 @@ class Animal:
     
 class Dog(Animal):
     def __init__(self, name, breed, color):
-        super().__init__(breed, color)# super() method calls the parent constructor in our case  Animal
+        super().__init__(breed, color)# super() method calls the parent constructor in our case  Animal and inherits everything
         self.name = name
     def __str__(self):
         return f"{self.name},{self.breed},{self.color}"
-    
-dog1 = Dog("JACK", "ROT", "Black")
-print(dog1)
+    #def __init__(self, name, breed):
+    #    Animal.__init__(self, breed, color="unknown")# calling animal directly inherits only a specific attribute, and in this case breed alone
+    #    self.name = name
+    #def __str__(self):
+    #    return f"{self.name},{self.breed}"
+#dog1= Dog("JACK", "ROT")
+#  
+dog2 = Dog("JACK", "ROT", "Black")
+print(dog2)
